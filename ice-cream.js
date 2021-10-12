6cacf7666fe40cdc24932abf331c6c7bd9adcdfd
Abdul Said <abdulrahman.asaid@gmail.com>
2021-10-12 02:17:36 +0000

    added array and for loop for packing list

--- ice-cream.js
+++ ice-cream.js
@@ -1,10 +1,6 @@
-// Do not modify these first two lines
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-console.log(iceCreamFlavours);
 
-// Your code below here...
-iceCreamFlavours.push("root beer");
-console.log(iceCreamFlavours);
-console.log(iceCreamFlavours[0]);
-console.log(iceCreamFlavours[5]);
-console.log(iceCreamFlavours.length)
+const areMultiples  = function (bigNumber, smallNumber) {
+  return bigNumber % smallNumber === 0;
+}
+
+console.log(areMultiples(15, 4));
