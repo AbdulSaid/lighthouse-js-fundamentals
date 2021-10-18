2ffc7249e1302c68c97f71a98e36983604d91008
Abdul Said <abdulrahman.asaid@gmail.com>
2021-10-18 03:57:19 +0000

    added urlEncode.js

--- judgeVegetable.js
+++ judgeVegetable.js
@@ -31,26 +31,3 @@ const judgeVegetable = function(vegetables,metric) {
 }
 
 console.log(judgeVegetable(vegetables, metric));
-
-const instructorWithLongestName = function(instructors) {
-  let longestName = ""
-  for (let i = 0; i < instructors.length; i++){
-    // let nameLength = instructors[i].name
-    if (instructors[i].name.length > longestName.length){
-      longestName = instructors[i].name
-    }
-  }
-  return longestName
-};
-
-console.log(instructorWithLongestName([
-  {name: "Samuel", course: "iOS"},
-  {name: "Jeremiah", course: "Web"},
-  {name: "Ophilia", course: "Web"},
-  {name: "Donald", course: "Web"}
-]));
-console.log(instructorWithLongestName([
-  {name: "Matthew", course: "Web"},
-  {name: "David", course: "iOS"},
-  {name: "Domascus", course: "Web"}
-]));
